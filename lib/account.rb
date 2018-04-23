3611820c8d2b19e42df95298af5e354aa859d309
Somayeh <mail.somayeh@gmail.com>
2018-04-23 15:05:18 +0100

    Write test for making deposit

--- lib/account.rb
+++ lib/account.rb
@@ -1,7 +1,11 @@
 class Account
 
+  def initialize(balance = 0)
+    @balance = balance
+  end
+
   def deposit(amount, date)
-    
+
   end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,9 +5,24 @@ describe Account do
   subject(:account) { described_class.new }
 
   describe "#deposit" do
-    it "acoount responds to deposit method" do
-      expect(account).to respond_to(:deposit).with(2).arguments
+    describe "acoount responds to deposit method" do
+      it "with two arguments" do
+        expect(account).to respond_to(:deposit).with(2).arguments
+      end
     end
+
+    describe "client can make a deposit to account" do
+      it "with specific amount and date" do
+        account.deposit(1000, 01-02-2018)
+        expect(account.balance).to eq(1000)
+      end
+    end
+
+
+
   end
+
+
+
 
 end
