bf95dfe5297a6234665ea92d67c3cb5ffbf132a9
Somayeh <mail.somayeh@gmail.com>
2018-04-24 14:12:22 +0100

    Test to add deposit to transactions array

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,13 +3,12 @@ describe Account do
   subject(:account) { described_class.new }
 
   describe '#deposit' do
-
     it 'Responds to deposit method' do
       expect(account).to respond_to(:deposit)
     end
 
     it 'Account starts with the balance of zero' do
-      expect(account.balance).to eq (0)
+      expect(account.balance).to eq(0)
     end
 
     it 'Client can make a deposit to account' do
@@ -17,13 +16,17 @@ describe Account do
       expect(account.balance).to eq(1000)
     end
 
+    it 'After making deposit add credit to transactions array' do
+      account.deposit(500)
+      expect(account.transactions).to eq([{ :credit => 500 }])
+    end
   end
 
   describe '#withdrawl' do
     it 'Client can withdrawl amount from account' do
       account.deposit(2000)
       account.withdrawl(500)
-      expect(account.balance).to eq (1500)
+      expect(account.balance).to eq(1500)
     end
 
     it 'Raise error for withdrawl if amount is more than balance' do
