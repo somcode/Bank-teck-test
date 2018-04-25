a5bb61a6a56afa133c624e3080a4ddc94b402bcd
Somayeh <mail.somayeh@gmail.com>
2018-04-25 11:40:39 +0100

    Print body method takes one argument

--- lib/print.rb
+++ lib/print.rb
@@ -2,4 +2,8 @@ class Print
   def print_header
     puts " date || credit || debit || balance "
   end
+
+  def print_body(transactions)
+    
+  end
 end
--- spec/print_spec.rb
+++ spec/print_spec.rb
@@ -6,4 +6,10 @@ describe Print do
       expect{ subject.print_header }.to output(" date || credit || debit || balance \n").to_stdout
     end
   end
+
+  describe '#print_body' do
+    it 'Responds to print method with one argument' do
+      expect(subject).to respond_to(:print_body).with(1).argument
+    end
+  end
 end
