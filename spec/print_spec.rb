2350ce3470dd4df743512c8677b643445b07d657
Somayeh <mail.somayeh@gmail.com>
2018-04-26 16:26:42 +0100

    Refactore print method

--- spec/print_spec.rb
+++ spec/print_spec.rb
@@ -1,25 +1,23 @@
 require 'print'
 describe Print do
-
   describe '#print_header' do
     it 'Print header for transactions' do
       expect{ subject.print_header }.to output(" date || credit || debit || balance \n").to_stdout
     end
   end
 
-  describe '#print_body' do
+  describe '#print' do
     it 'Responds to print method with one argument' do
-      expect(subject).to respond_to(:print_body).with(1).argument
+      expect(subject).to respond_to(:print).with(1).argument
     end
 
     it 'Print the transaction include date, credit, debit, balance' do
       expect do
-        subject.print_body([
-          " date || 500 ||  || 500 ",
-           " date ||  || 800 || 800 "
+        subject.print([
+          ' date || 500.00 ||  || 500.00 ',
+          ' date ||  || 800.00 || 800.00 '
            ])
-         end.to output(" date ||  || 800 || 800 \n date || 500 ||  || 500 \n").to_stdout
-
+         end.to output(" date || credit || debit || balance \n date ||  || 800.00 || 800.00 \n date || 500.00 ||  || 500.00 \n").to_stdout
     end
   end
 
