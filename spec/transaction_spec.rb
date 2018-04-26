511400c251a878795b191bda29fa47c17e3f1e1b
Somayeh <mail.somayeh@gmail.com>
2018-04-26 11:21:13 +0100

    Test to add transaction to list

---
+++ spec/transaction_spec.rb
@@ -0,0 +1,9 @@
+describe Transaction do
+  describe '#add' do
+    context 'After each transaction' do
+      it 'Add transaction to list' do
+        expect(subject.add("2018-04-03 || 500.00 ||  || 500.00 ")).to eq (["2018-04-03 || 500.00 ||  || 500.00 "])
+      end
+    end
+  end
+end
